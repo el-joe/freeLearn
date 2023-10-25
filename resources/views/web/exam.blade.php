718be9c17d5ba0518032ff9ae057b3042f3193cb
Joe <eljoe1717@gmail.com>
2023-10-25 17:19:28 +0300

    Fixes

--- resources/views/web/exam.blade.php
+++ resources/views/web/exam.blade.php
@@ -1,46 +1,54 @@
 @extends('web.layout.index')
 
 @section('content')
-    <form action="{{route('submitExam',$subId)}}" method="POST">
-        @csrf
-        <div class="container px-4 lg:px-8 mx-auto h-full max-w-screen-xl text-gray-700">
-            @foreach ($lesson->questions as $key => $q)
-                <div class="flex flex-col mt-20 gap-10">
-                    <div class="relative mt-20 sm:mt-0">
-                        <span class="tex-green-500 text-2xl font-medium">Question {{ $key + 1 }}</span>
-                        @if ($q->type == 'image')
-                            <img class="bg-white bg-opacity-80 rounded-lg" src="{{ $q->file?->file_path }}" width="200"
-                                height="50" />
-                        @else
-                            <span class="tex-green-500 text-2xl font-medium">{{ $q->source }}</span>
-                        @endif
-                    </div>
 
-                    <div class="flex flex-row gap-5">
-                        <!-- radion buttons  -->
-                        @foreach ($q->options as $key => $option)
-                            <label for="slider{{ $option->id }}">
-                                <input type="radio" name="question[{{ $q->id }}]" value="{{ ++$key }}"
-                                    id="slider{{ $option->id }}" />
-                                <br>
-                                @if ($option->type == 'image')
-                                    <img class="bg-white bg-opacity-80 rounded-lg" src="{{ $option->file?->file_path }}"
-                                        width="200" height="50" />
-                                @else
-                                    {{ $option->source }}
-                                @endif
-                            </label>
-                        @endforeach
-                    </div>
+    <form id="regForm" class="container" action="{{ route('submitExam', $subId) }}" method="POST">
+        @csrf
+        <h1 style="font-size: 50px;font-weight: bolder">Exam Questions : </h1>
+
+        @foreach ($lesson->questions as $key => $q)
+            <div class="flex flex-col mt-20 gap-10 tab">
+                <div class="relative mt-20 sm:mt-0">
+                    <span class="tex-green-500 text-2xl font-medium">Question {{ $key + 1 }}</span>
+                    @if ($q->type == 'image')
+                        <img class="bg-white bg-opacity-80 rounded-lg" src="{{ $q->file?->file_path }}" />
+                    @else
+                        <span class="tex-green-500 text-2xl font-medium">{{ $q->source }}</span>
+                    @endif
                 </div>
+
+                <div class="flex flex-row gap-5">
+                    <!-- radion buttons  -->
+                    @foreach ($q->options as $key => $option)
+                        <label for="slider{{ $option->id }}">
+                            <input type="radio" name="question[{{ $q->id }}]" value="{{ ++$key }}"
+                                id="slider{{ $option->id }}" />
+                            <br>
+                            @if ($option->type == 'image')
+                                <img class="bg-white bg-opacity-80 rounded-lg" src="{{ $option->file?->file_path }}"
+                                    width="200" height="50" />
+                            @else
+                                {{ $option->source }}
+                            @endif
+                        </label>
+                    @endforeach
+                </div>
+            </div>
+        @endforeach
+        <div style="overflow:auto;">
+            <div style="float:right;">
+                <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+                <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+            </div>
+        </div>
+        <!-- Circles which indicates the steps of the form: -->
+        <div style="text-align:center;margin-top:40px;">
+        @foreach ($lesson->questions as $key => $q)
+            <span class="step"></span>
             @endforeach
-            <br>
-            <br>
-            <br>
-            <br>
-            <button class="text-gray-700 bg-cream rounded-full px-4 py-2 font-semibold text-sm">Submit Exam</button>
         </div>
     </form>
+
     <div id="image-viewer">
         <span class="close">&times;</span>
         <img class="modal-content" id="full-image">
@@ -50,13 +58,14 @@
 @push('styles')
     <style>
         /* Style the Image Used to Trigger the Modal */
-        img {
+        .container img {
             border-radius: 5px;
             cursor: pointer;
             transition: 0.3s;
+            width: 200px;height: 50px;
         }
 
-        img:hover {
+        .container img:hover {
             opacity: 0.7;
         }
 
@@ -73,6 +82,7 @@
             overflow: auto;
             background-color: rgb(0, 0, 0);
             background-color: rgba(0, 0, 0, 0.9);
+            opacity: 99999;
         }
 
         .modal-content {
@@ -120,6 +130,88 @@
             }
         }
     </style>
+
+    <style>
+
+        * {
+        box-sizing: border-box;
+        }
+        #regForm {
+            background-color: #ffffff;
+            margin: 100px auto;
+            font-family: Raleway;
+            padding: 40px;
+            width: 70%;
+            min-width: 300px;
+        }
+
+        .tab {
+            border: 1px solid #cccccc;
+            padding: 50px;
+            margin: 50px 0;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        input {
+            padding: 10px;
+            width: 100%;
+            font-size: 17px;
+            font-family: Raleway;
+            border: 1px solid #aaaaaa;
+        }
+
+        /* Mark input boxes that gets an error on validation: */
+        input.invalid {
+            background-color: #ffdddd;
+        }
+
+        /* Hide all steps by default: */
+        .tab {
+            display: none;
+        }
+
+        button {
+            background-color: #04AA6D;
+            color: #ffffff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 17px;
+            font-family: Raleway;
+            cursor: pointer;
+        }
+
+        button:hover {
+            opacity: 0.8;
+        }
+
+        #prevBtn {
+            background-color: #bbbbbb;
+        }
+
+        /* Make circles that indicate the steps of the form: */
+        .step {
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbbbbb;
+            border: none;
+            border-radius: 50%;
+            display: inline-block;
+            opacity: 0.5;
+        }
+
+        .step.active {
+            opacity: 1;
+        }
+
+        /* Mark the steps that are finished and valid: */
+        .step.finish {
+            background-color: #04AA6D;
+        }
+    </style>
 @endpush
 
 @push('scripts')
@@ -129,11 +221,87 @@
     <script>
         $(".container img").click(function() {
             $("#full-image").attr("src", $(this).attr("src"));
+            alert($(this).attr("src"));
             $('#image-viewer').show();
         });
 
         $("#image-viewer .close").click(function() {
             $('#image-viewer').hide();
         });
+    </script>
+
+<script>
+    var currentTab = 0; // Current tab is set to be the first tab (0)
+    showTab(currentTab); // Display the current tab
+
+    function showTab(n) {
+      // This function will display the specified tab of the form...
+      var x = document.getElementsByClassName("tab");
+      x[n].style.display = "block";
+      //... and fix the Previous/Next buttons:
+      if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+      } else {
+        document.getElementById("prevBtn").style.display = "inline";
+      }
+      if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+      } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+      }
+      //... and run a function that will display the correct step indicator:
+      fixStepIndicator(n)
+    }
+
+    function nextPrev(n) {
+      // This function will figure out which tab to display
+      var x = document.getElementsByClassName("tab");
+      // Exit the function if any field in the current tab is invalid:
+      if (n == 1 && !validateForm()) return false;
+      // Hide the current tab:
+      x[currentTab].style.display = "none";
+      // Increase or decrease the current tab by 1:
+      currentTab = currentTab + n;
+      // if you have reached the end of the form...
+      if (currentTab >= x.length) {
+        // ... the form gets submitted:
+        document.getElementById("regForm").submit();
+        return false;
+      }
+      // Otherwise, display the correct tab:
+      showTab(currentTab);
+    }
+
+    function validateForm() {
+      // This function deals with validation of the form fields
+      var x, y, i, valid = true;
+      x = document.getElementsByClassName("tab");
+      y = x[currentTab].getElementsByTagName("input");
+      // A loop that checks every input field in the current tab:
+      for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == "") {
+          // add an "invalid" class to the field:
+          y[i].className += " invalid";
+          // and set the current valid status to false
+          valid = false;
+        }
+      }
+      // If the valid status is true, mark the step as finished and valid:
+      if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+      }
+      return valid; // return the valid status
+    }
+
+    function fixStepIndicator(n) {
+      // This function removes the "active" class of all steps...
+      var i, x = document.getElementsByClassName("step");
+      for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+      }
+      //... and adds the "active" class on the current step:
+      x[n].className += " active";
+    }
     </script>
 @endpush
